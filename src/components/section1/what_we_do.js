1ed0fabb6918f0fa14f8e7dfe8d0d9aebf7a8227
tony <owner@owners-mbp.attlocal.net>
2022-06-04 07:20:34 -0700

    added background

---
+++ src/components/section1/what_we_do.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class Section1 extends Component {
+  render() {
+    return (
+      <div>
+       <h1>Dui augue lectus in ut at lorem</h1>
+        
+      </div>
+    );
+  }
+}
