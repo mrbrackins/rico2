d4f2dbae836dad1707df860c564fd2d700a7e9b3
Dad <dad@brackinss-imac.attlocal.net>
2022-06-06 07:59:38 -0700

    started review

--- src/components/section1/review.js
+++ src/components/section1/review.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Review extends Component {
   render() {
     return (
-      <div>
+      <div className="reviewSection">
         <h1>Review</h1>
       </div>
     );
